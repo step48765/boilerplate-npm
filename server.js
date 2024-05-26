c4868feec1187d1a9f5fd9aa1558f4aac277b8eb
Michael Grant Warshowksy <mwarshowsky71@gmail.com>
2024-05-26 17:57:32 -0400

    forgot to save the last file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
+    var origin = req.headers.origin || '*'; 
     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
          console.log(origin);
          res.setHeader('Access-Control-Allow-Origin', origin);
