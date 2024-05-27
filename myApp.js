fdb3caa7c6faf4c598f163eade24307d837949aa
Michael Grant Warshowksy <mwarshowsky71@gmail.com>
2024-05-27 13:32:08 -0400

    fix so it check the case of the env var

--- myApp.js
+++ myApp.js
@@ -13,10 +13,10 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 app.get("/json", (req, res) => {
-    if(process.env.MESSAGE_STYLE == "uppercase"){
-        res.json({"message": "Hello json"});
-    }else{
+    if(process.env.MESSAGE_STYLE == process.env.MESSAGE_STYLE.toUpperCase()){
         res.json({"message": "HELLO JSON"});
+    }else{
+        res.json({"message": "Hello json"});
     }
     
 });
