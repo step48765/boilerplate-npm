de5c2a4b94b7b25d57b9121d71328a36b74c5f59
Michael Grant Warshowksy <mwarshowsky71@gmail.com>
2024-05-27 14:17:17 -0400

    add middlewear time breakup

--- myApp.js
+++ myApp.js
@@ -27,4 +27,12 @@ app.use((req, res, next) =>{
     next();
 });
 
+app.get("/now", (req, res, next) => {
+      req.time = new Date().toString();
+      next();
+    }, (req, res) => {
+      res.send({time: req.time});
+    }
+  );
+
 module.exports = app;
